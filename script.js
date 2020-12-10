1182104f8ceb2bbf22a7dac46f73b983ea09f33f
Dylan <55002846+ohitsdylan@users.noreply.github.com>
2020-12-10 14:57:29 -0500

    add script to disable submit button if request or comment body is empty
    this includes styling elements
    other changes are me formatting code

--- script.js
+++ script.js
@@ -122,22 +122,25 @@ $(document).ready(function() {
 		}
     });
 
-    // Pulled two functions from Zendesk's Copenhagen theme to operate the Status filter
+    // Pulled a few functions from Zendesk's Copenhagen theme
     // (https://github.com/zendesk/copenhagen_theme/blob/master/script.js)
 
     // Function 1 - closest()
     function closest (element, selector) {
         if (Element.prototype.closest) {
-          return element.closest(selector);
+            return element.closest(selector);
         }
         do {
-          if (Element.prototype.matches && element.matches(selector)
+            if (Element.prototype.matches && element.matches(selector)
             || Element.prototype.msMatchesSelector && element.msMatchesSelector(selector)
             || Element.prototype.webkitMatchesSelector && element.webkitMatchesSelector(selector)) {
-            return element;
-          }
-          element = element.parentElement || element.parentNode;
+                return element;
+            }
+
+            element = element.parentElement || element.parentNode;
+
         } while (element !== null && element.nodeType === 1);
+
         return null;
     }
 
@@ -147,7 +150,9 @@ $(document).ready(function() {
         var activeElementId = document.activeElement.getAttribute("id");
         sessionStorage.setItem('returnFocusTo', '#' + activeElementId);
     }
+
     var returnFocusTo = sessionStorage.getItem('returnFocusTo');
+
     if (returnFocusTo) {
         sessionStorage.removeItem('returnFocusTo');
         var returnFocusToEl = document.querySelector(returnFocusTo);
@@ -157,43 +162,67 @@ $(document).ready(function() {
     // Submit requests filter form when the Status drop down menu is changed
     Array.prototype.forEach.call(document.querySelectorAll('#request-status-select'), function(el) {
         el.addEventListener('change', function(e) {
-        e.stopPropagation();
-        saveFocus();
-        closest(this, 'form').submit();
+            e.stopPropagation();
+            saveFocus();
+            closest(this, 'form').submit();
         });
     });
 
     // Submit requests filter form on search in the request list page
     var quickSearch = document.querySelector('#quick-search');
+
     quickSearch && quickSearch.addEventListener('keyup', function(e) {
         if (e.keyCode === ENTER) {
-        e.stopPropagation();
-        saveFocus();
-        closest(this, 'form').submit();
+            e.stopPropagation();
+            saveFocus();
+            closest(this, 'form').submit();
         }
     });
 
     // Submit requests filter form on status or organization change in the request list page
     Array.prototype.forEach.call(document.querySelectorAll('#request-status-select, #request-organization-select'), function(el) {
         el.addEventListener('change', function(e) {
-        e.stopPropagation();
-        saveFocus();
-        closest(this, 'form').submit();
+            e.stopPropagation();
+            saveFocus();
+            closest(this, 'form').submit();
         });
     });
-
 
     // Submit organization form in the request page
     var requestOrganisationSelect = document.querySelector('#request-organization select');
 
     if (requestOrganisationSelect) {
         requestOrganisationSelect.addEventListener('change', function() {
-        closest(this, 'form').submit();
+            closest(this, 'form').submit();
         });
     }
-
   
     // End borrowed Zendesk scripts
+
+    // Disable Submit button if text area is empty
+    $( '.request_body footer input' ).prop( 'disabled', true );
+
+    $( '#request_description' ).change( function() {
+        if ( !$.trim( $( '#request_description' ).val()) ) {
+            $( '.request_body footer input' ).prop( 'disabled', true );
+            $( '#request_description_hint' ).css( 'color', '#ff4c5b');
+        } else {
+            $( '.request_body footer input' ).prop( 'disabled', false );
+            $( '#request_description_hint' ).css( 'color', '#a7a7a7');
+        }
+    });
+
+
+    $( '.ticket_reply-controls_submit input' ).prop( 'disabled', true );
+
+    $( '#request_comment_body' ).change( function() {
+        if ( !$.trim( $( '#request_comment_body' ).val()) ) {
+            $( '.ticket_reply-controls_submit input' ).prop( 'disabled', true );
+        } else {
+            $( '.ticket_reply-controls_submit input' ).prop( 'disabled', false );
+        }
+    });
+    
 
     // Hide the "Internal Support" links in the User Profile page if not Agent
     if (HelpCenter.user.role == 'agent') {
