0f6f51e20b2551130f36ef80ca45b253990476c6
Dylan <55002846+ohitsdylan@users.noreply.github.com>
2020-12-17 11:20:59 -0500

    add scroll function for article anchor tags

--- script.js
+++ script.js
@@ -212,7 +212,7 @@ $(document).ready(function() {
         }
     });
 
-
+    // Also do the same for the in-ticket reply
     $( '.ticket_reply-controls_submit input' ).prop( 'disabled', true );
 
     $( '#request_comment_body' ).change( function() {
@@ -222,7 +222,6 @@ $(document).ready(function() {
             $( '.ticket_reply-controls_submit input' ).prop( 'disabled', false );
         }
     });
-    
 
     // Hide the "Internal Support" links in the User Profile page if not Agent
     if (HelpCenter.user.role == 'agent') {
@@ -256,4 +255,46 @@ $(document).ready(function() {
 
     // Also wrap tables in a responsive div tag
     $( '.article_body table' ).wrap( '<div class="table-responsive"></div>' );
+
+    // Scroll article anchor tags into view
+    // from: https://css-tricks.com/snippets/jquery/smooth-scrolling/
+    $('a[href*="#"]')
+
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+
+    .click(function(event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+            && 
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                    $('html, body').animate({
+                        // EDIT: offset scroll by 50 pixels to account for Zendesk's admin bar
+                        scrollTop: target.offset().top - 50
+                }, 1000, function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) { // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    };
+                });
+            }
+        }
+    });
 });
