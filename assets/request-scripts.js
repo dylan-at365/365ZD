fe0f6ba2730ee23f62debed146beabb211593cdd
Dylan <55002846+ohitsdylan@users.noreply.github.com>
2021-09-21 09:05:25 -0400

    fix submit button disable for new requests

--- assets/request-scripts.js
+++ assets/request-scripts.js
@@ -17,4 +17,20 @@ $(document).ready(function() {
         }
     });
 
+    // Do the same for the `new_request_page.hbs` template
+    // TODO: This can probably be combined with the script above, somehow. An OR selector for the jQuery find?
+    $( '#request_description' ).change( function() {
+        if ( !$.trim( $( 'textarea' ).val()) ) {
+            $( '.request-container input[name="commit"]' ).prop( 'disabled', true );
+            $( '.request-container input[name="commit"]' ).addClass('button--inactive');
+        } else {
+            $( '.request-container input[name="commit"]' ).prop( 'disabled', false );
+            $( '.request-container input[name="commit"]' ).removeClass('button--inactive');
+        }
+    });
+
+
+
+
+
 });
