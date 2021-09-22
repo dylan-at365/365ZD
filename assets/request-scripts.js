cf9258ed942ff74a2a7e2a6f373b469fcec74144
Dylan <55002846+ohitsdylan@users.noreply.github.com>
2021-09-22 13:34:17 -0400

    fix mark as solved button
    imported and adjusted code from Zendesk's Copenhagen theme to make the "Mark as Solved" button function

--- assets/request-scripts.js
+++ assets/request-scripts.js
@@ -1,19 +1,26 @@
 $(document).ready(function() {
+
+    var markAsSolvedButton = $( '.mark-as-solved' ),
+        markAsSolvedCheckbox = $( 'input[name="mark_as_solved"]' ),
+        commentSubmitButton = $( '.request-container input[name="commit"]' );
     
     // Disable Submit button if text area is empty
     if ( $( '.request-container' ) && $( '.request-container' ).val() === '') {
         $( '.request-container input[name="commit"]' ).prop( 'disabled', true );
-        $( '.request-container input[name="commit"]' ).addClass('button--inactive');
+        $( '.request-container input[name="commit"]' ).addClass( 'button--inactive' );
     }
 
     // Watch for changes in the `textarea` element and adjust the button status accordingly
     $( '#request_comment_body' ).change( function() {
         if ( !$.trim( $( 'textarea' ).val()) ) {
-            $( '.request-container input[name="commit"]' ).prop( 'disabled', true );
-            $( '.request-container input[name="commit"]' ).addClass('button--inactive');
+            commentSubmitButton.prop( 'disabled', true );
+            commentSubmitButton.addClass( 'button--inactive' );
+            markAsSolvedButton.html(markAsSolvedButton.attr( 'data-solve-translation' ));
+
         } else {
-            $( '.request-container input[name="commit"]' ).prop( 'disabled', false );
-            $( '.request-container input[name="commit"]' ).removeClass('button--inactive');
+            commentSubmitButton.prop( 'disabled', false );
+            commentSubmitButton.removeClass( 'button--inactive' );
+            markAsSolvedButton.html(markAsSolvedButton.attr( 'data-solve-and-submit-translation' ));
         }
     });
 
@@ -21,16 +28,21 @@ $(document).ready(function() {
     // TODO: This can probably be combined with the script above, somehow. An OR selector for the jQuery find?
     $( '#request_description' ).change( function() {
         if ( !$.trim( $( 'textarea' ).val()) ) {
-            $( '.request-container input[name="commit"]' ).prop( 'disabled', true );
-            $( '.request-container input[name="commit"]' ).addClass('button--inactive');
+            commentSubmitButton.prop( 'disabled', true );
+            commentSubmitButton.addClass( 'button--inactive' );
         } else {
-            $( '.request-container input[name="commit"]' ).prop( 'disabled', false );
-            $( '.request-container input[name="commit"]' ).removeClass('button--inactive');
+            commentSubmitButton.prop( 'disabled', false );
+            commentSubmitButton.removeClass( 'button--inactive' );
         }
     });
 
-
-
-
+    if (markAsSolvedButton) {
+        markAsSolvedButton.click( function() {
+            markAsSolvedCheckbox.prop( 'checked', true );
+            commentSubmitButton.prop( 'disabled', true );
+            $(this).data( 'data-disabled', true );
+            $(this).closest( 'form' ).submit();
+        });
+    };
 
 });
