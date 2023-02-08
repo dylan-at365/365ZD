1a572c3f601ed2ec22f1e4937f96fc8b0d9e36c3
Dylan <55002846+ohitsdylan@users.noreply.github.com>
2023-02-08 16:01:53 -0500

    clean up color mode toggle scripts

--- assets/toggle-color-mode.js
+++ assets/toggle-color-mode.js
@@ -7,22 +7,6 @@
 
 $(document).ready(function() {
 
-    // Remove the mode toggle/clear elements if user is not internal (for now)
-    if ( HelpCenter.user.organizations[0] === undefined ) {
-        $( '.mode-toggle' ).remove();
-        $( '.clear-mode-toggle' ).remove();
-    } 
-    
-    else if ( HelpCenter.user.organizations[0].name == '365 Retail Markets' ) {
-        $( '.mode-toggle' ).show();
-        $( '.clear-mode-toggle' ).show();
-    } 
-    
-    else {
-        $( '.mode-toggle' ).remove();
-        $( '.clear-mode-toggle' ).remove();
-    };
-
     // Update the toggle switch button based on the selected color scheme, either from the OS or the toggle
     // TODO: Using the `updateToggleButton` function as it is run on each page load. Could maybe use something more specific to the localStorage check? At least rename the function...
     function updateToggleButton() {
@@ -101,7 +85,7 @@ $(document).ready(function() {
     }
 
     // Toggle button click
-    $( '#js-mode-toggle-switch' ).bind( 'click', function() {
+    $( '#js-mode-toggle-switch' ).on( 'click', function() {
 
         // Get current mode
         $mode = $( '#css' ).attr('data-color-scheme');
@@ -131,62 +115,6 @@ $(document).ready(function() {
             $( this ).removeClass( 'js-rotate-button' );
         });
         
-
-    });
-
-    // Clear the localStorage if the user wants to use the OS-defined setting again
-    $( '#js-clear-mode-storage' ).bind( 'click', function() {
-
-        // If nothing has been set and the button is clicked, fire a toast message informing the user
-        if ( !localStorage.getItem( 'colorMode' ) ) {
-
-            $( '.toast' ).addClass( 'toast--alert' );
-            $( '.toast__title' ).text( 'Error' );
-            $( '.toast__description' ).text( 'Theme mode toggle not set!' );
-
-            $( '.toast' ).removeClass( 'd-none' );
-            $( '.toast' ).animate( { opacity: 1 }, 200 );
-
-            // Is a timeout inside a timeout a bad idea? The styles get removed from the fading element too early otherwise...
-            setTimeout(() => {
-                $( '.toast' ).animate( { opacity: 0 }, 200 );
-
-                setTimeout(() => {
-                    $( '.toast' ).removeClass( 'toast--alert' );
-                    $( '.toast' ).addClass( 'd-none' );
-                }, 200 );
-            }, 3000 );
-
-        }
-
-        // If localStorage exists with the theme selection, remove it
-        else {
-
-            localStorage.removeItem( 'colorMode' );
-
-            // Inform the user that something happened
-            $( '.toast' ).addClass( 'toast--success' );
-            $( '.toast__title' ).text( 'Theme mode cleared' );
-            $( '.toast__description' ).text( 'Please refresh the page to view changes.');
-
-            $( '.toast' ).removeClass( 'd-none' );
-            $( '.toast' ).animate( { opacity: 1 }, 200 );
-
-            setTimeout(() => {
-                $( '.toast' ).animate( { opacity: 0 }, 200 );
-                
-                setTimeout(() => {
-                    $( '.toast' ).removeClass( 'toast--success' );
-                    $( '.toast' ).addClass( 'd-none' );
-                }, 200 );
-
-            }, 3000 );
-
-            // Set our "clear" button back to "inactive"
-            $( '.clear-mode-toggle' ).addClass( 'button--inactive' );
-
-        }
-
     });
 
 });
